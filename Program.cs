40017d77b8865af2dd9a1deb977f7687b78be7d7
katevolved3@mail.ru <katevolved3@mail.ru>
2023-06-19 00:17:16 +0700

    Ready.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+
+namespace lab5
+{
+    
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите путь к текстовому файлу: ");
+            string Path = Console.ReadLine();
+            OhWords.FixFile(Path);
+            Console.WriteLine("файл исправлен\n");
+            Console.ReadKey();
+        }
+    }
+}
